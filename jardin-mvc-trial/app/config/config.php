bb8eda9c3a7a5643d9182e289d849a9f22b8a6e4
zeinadesouky <76789833+zeinadesouky@users.noreply.github.com>
2021-06-19 22:31:27 +0200

    Add files via upload

---
+++ jardin-mvc-trial/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','jardin');
+
+ 
+ define ('APPROOT', dirname(dirname(__FILE__)));
+
+ define ('URLROOT', 'localhost/jardin-mvc');
+
+ define ('SITENAME', 'Jardin');
